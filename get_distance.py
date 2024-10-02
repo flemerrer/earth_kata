235e36c6442654586d3df95c2e0e2c1e9ab42dfa
flemerrer <francois.lemerrer@protonmail.com>
2024-10-02 12:52:19 +0200

    write first test

--- get_distance.py
+++ get_distance.py
@@ -1,1 +1,8 @@
 
+import math
+
+def two_points_distance_calculation(coordinates_x, coordinates_y):
+    pass
+
+def find_max_distance():
+    pass
---
+++ test_get_distance.py
@@ -0,0 +1,13 @@
+from get_distance import two_points_distance_calculation
+
+
+def test_two_points_distance_calculation_should_return_the_hypotenuse_value_of_x_y_rectangle_triangle():
+    position1 = (30, 50)
+    position2 = (40, 60)
+
+    distance = two_points_distance_calculation(position1, position2)
+
+    assert round(distance, 3) == round(14.1421, 3)
+    
+def test_find_max_distance_should_return_the_greatest_value_from_input_distances():
+    pass
