1b002921443b1622e5ce804514ab5ee2ef57fec6
flemerrer <francois.lemerrer@protonmail.com>
2024-10-02 12:53:40 +0200

    add first test implementation

--- get_distance.py
+++ get_distance.py
@@ -2,7 +2,14 @@
 import math
 
 def two_points_distance_calculation(coordinates_x, coordinates_y):
-    pass
+    xa = coordinates_x[0]
+    ya = coordinates_x[1]
+    xb = coordinates_y[0]
+    yb = coordinates_y[1]
+
+    distance_squared = (xa - xb)**2 + (ya - yb)**2
+
+    return math.sqrt(distance_squared)
 
 def find_max_distance():
     pass
