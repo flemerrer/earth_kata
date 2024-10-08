93a1732e41d9e1f2fe5f8e5c1fd49aa527562600
flemerrer <francois.lemerrer@protonmail.com>
2024-10-08 16:21:22 +0200

    refactor location class and add co

--- get_distance.py
+++ get_distance.py
@@ -2,13 +2,27 @@ import json, requests
 
 
 class Location:
-    def __init__(self, lati, long, name='', elev=0):
+    def __init__(self, lati, long, elev=0, name=''):
         self.name = name
-        self.coordinates = (lati, long)
+        self.latitude = lati
+        self.longitude = long
         self.elevation = elev
+
+    def get_coordinates(self):
+        return self.latitude, self.longitude
 
     def set_elevation(self, elevation):
         self.elevation = elevation
+
+
+class Couple:
+    def __init__(self, point_a, point_b):
+        self.point: Location = point_a
+        self.antipodal_point: Location = point_b
+
+    # earth_diameter is about 12742 km
+    def diameter_calculation(self):
+        return 12742000 + self.point.elevation + self.antipodal_point.elevation
 
 
 def get_capitals_list(dict):
@@ -24,8 +38,8 @@ def find_antipodal_points(locations_list):
     antipodal_points_list = []
 
     for location in locations_list:
-        coordinates = find_antipodal_point(location.coordinates)
-        antipodal_points_list.append(Location(coordinates[0], coordinates[1], f'Opposite of {location.name}'))
+        coordinates = find_antipodal_point(location)
+        antipodal_points_list.append(Location(coordinates[0], coordinates[1], 0, f'Opposite of {location.name}'))
 
     return antipodal_points_list
 
@@ -35,29 +49,28 @@ def create_diameters_list(locations, antipodals):
 
     for elem in locations:
         i = locations.index(elem)
-        diameter = diameter_calculation(elem.elevation, antipodals[i].elevation)
+        couple = Couple(locations[i], antipodals[i])
+        diameter = couple.diameter_calculation()
         diameters.append((f'{elem.name} <-> {antipodals[i].name} Distance', diameter))
 
     return diameters
 
 
-def find_antipodal_point(coordinates):
-    latitude = coordinates[0]
-
-    if coordinates[1] > 0:
-        longitude = 180 - coordinates[1]
+def find_antipodal_point(location):
+    if location.longitude > 0:
+        longitude = 180 - location.longitude
     else:
-        longitude = 180 - abs(coordinates[1])
+        longitude = 180 - abs(location.longitude)
         longitude = -longitude
 
-    return -latitude, -longitude
+    return -location.latitude, -longitude
 
 
 # todo: make it so that get_elevation only sends one request for any given list of coordinates
 
-def get_elevation(coordinates):
+def get_elevation(location):
     response = requests.get(
-        f'https://api.open-elevation.com/api/v1/lookup?locations={coordinates[0]},{coordinates[1]}').json()
+        f'https://api.open-elevation.com/api/v1/lookup?locations={location.latitude},{location.longitude}').json()
 
     return response['results'][0]['elevation']
 
@@ -78,13 +91,6 @@ def import_equatorial_points():
     #todo: add exception handling ?
 
     return True
-
-# earth_diameter is about 12742 km
-def diameter_calculation(elevation_a, elevation_b, earth_diameter=12742000):
-    elevation_difference = elevation_a + elevation_b
-
-    return earth_diameter + elevation_difference
-
 
 def find_max_distance(diameters_list):
     max_distance = 0
--- test_get_distance.py
+++ test_get_distance.py
@@ -1,19 +1,29 @@
 import pytest
 
-from get_distance import diameter_calculation, find_max_distance, find_antipodal_point, get_elevation, Location, \
-    find_antipodal_points, create_diameters_list, get_capitals_list
+from get_distance import find_max_distance, find_antipodal_point, get_elevation, Location, \
+    find_antipodal_points, create_diameters_list, get_capitals_list, Couple
+
 
 # Reminder about tests : they must be simple to avoid them being false / wrong, and because they are a form of documentation. They must show how functions work ; not be another implementation of tested functions. If they have the same complexity level as the tested function, it is not right.
 # To help achieving that, use hard coded values in test. And like in any docu!entation, choose values to test that are not abstract. It facilitates comprehension (Alice and Bob > A & B)
 
 
+# @pytest.mark.parametrize({"location", "antipodal", "expected_distance"},
+#                          [
+#                              {location_a, antipodal_b, 12742515},
+#                              {location_b, antipodal_b, 12742545},
+#                          ])
 def test_diameter_calculation_should_return_the_distance_between_two_points():
-    elevation1 = 100
-    elevation2 = -50
+    location_1 = Location(10, 10, 515)
+    antipodal_a = Location(-10, -170)
+    location_2 = Location(-20, -20, 545)
+    antipodal_b = Location(20, 160)
 
-    distance = diameter_calculation(elevation1, elevation2)
+    couple_a = Couple(location_1, antipodal_a)
+    couple_b = Couple(location_2, antipodal_b)
 
-    assert distance == 12742050
+    assert couple_a.diameter_calculation() == 12742515
+    assert couple_b.diameter_calculation() == 12742545
 
 
 def test_find_max_distance_should_return_the_greatest_value_from_input_distances():
@@ -28,19 +38,18 @@ def test_find_max_distance_should_return_the_greatest_value_from_input_distances
     assert result == ('distance 3', 10)
 
 
-@pytest.mark.parametrize({"given_coordinates", "expected_coordinates"},
-                         [
-                             {(40, -5), (-40, 175)},
-                             {(49, 7), (-49, -173)},
-                             {(-32, 52), (32, -128)},
-                             {(75, 140), (-75, -40)},
-                             {(-25, -50), (25, 130)},
-                         ])
-def test_find_opposite_point_should_return_the_antipodal_point_coordinates_of_given_point_coordinates(given_coordinates,
-                                                                                                      expected_coordinates):
-    result_coordinates = find_antipodal_point(given_coordinates)
+# @pytest.mark.parametrize({"given_locations", "expected_coordinates"},
+#                          [
+#                              {Location(40, -5), (-40, 175)},
+#                              {Location(49, 7), (-49, -173)},
+#                              {Location(-32, 52), (32, -128)},
+#                              {Location(75, 140), (-75, -40)},
+#                              {Location(-25, -50), (25, 130)},
+#                          ])
+def test_find_antipodal_point_should_return_the_antipodal_point_coordinates():
+    location = Location(40, -5)
 
-    assert result_coordinates == expected_coordinates
+    assert find_antipodal_point(location) == (-40, 175)
 
 
 # pay attention and insert a backspace after a comma while listing ints ; if not, it'll be considered as one float !
@@ -59,7 +68,7 @@ def test_find_opposite_point_should_return_the_antipodal_point_coordinates_of_gi
 
 #@requests_mock.Mocker()
 def test_get_elevation_should_return_a_valid_json(mocker):
-    test_coordinates = (10, 10)
+    test_coordinates = Location(10, 10)
     test_data = {"results":[{"latitude":10.0,"longitude":10.0,"elevation":515.0}]}
 
     mock_response = mocker.MagicMock()
@@ -67,14 +76,12 @@ def test_get_elevation_should_return_a_valid_json(mocker):
 
     mocker.patch("requests.get", return_value=mock_response)
 
-    result = get_elevation(test_coordinates)
-
     # version that tests the whole data response
     # assert result == test_data
     # assert type(result) == dict
     # assert result["results"][0]["elevation"] == 515
 
-    assert result == 515
+    assert get_elevation(test_coordinates) == 515
 
 def test_find_antipodal_points_should_return_a_list_of_opposite_locations():
     paris = Location(49, 2, 'Paris')
@@ -84,9 +91,9 @@ def test_find_antipodal_points_should_return_a_list_of_opposite_locations():
 
     antipodal_locations = find_antipodal_points(locations)
 
-    assert antipodal_locations[0].coordinates == (-49, -178)
-    assert antipodal_locations[1].coordinates == (35, -31)
-    assert antipodal_locations[2].coordinates == (12, 103)
+    assert antipodal_locations[0].get_coordinates() == (-49, -178)
+    assert antipodal_locations[1].get_coordinates() == (35, -31)
+    assert antipodal_locations[2].get_coordinates() == (12, 103)
 
 
 # I started by coding a function that returned a list using a for loop on dictionary but I refactored it later
@@ -99,15 +106,15 @@ def test_get_locations_list_should_return_a_list_of_location_from_dictionary():
 
 def test_create_diameters_list_should_return_a_list_of_tuples():
     capitals_coordinates = {
-        'Paris': Location(49, 2, 'Paris', 182),
-        'London': Location(51, -0, 'London', ),
-        'New York': Location(41, -74, 'New York', ),
-        'Mexico': Location(19, -99, 'Mexico', ),
-        'Rio de Janeiro': Location(-23, -43, 'Rio de Janeiro'),
-        'Lima': Location(-12, -77, 'Lima', 221),
-        'New Delhi': Location(28, 77, 'New Delhi'),
-        'Beijin': Location(22, 113, 'Beijin'),
-        'Canberra': Location(-35, 149, 'Canberra'),
+        'Paris': Location(49, 2, 182, 'Paris', ),
+        'London': Location(51, -0, 0, 'London', ),
+        'New York': Location(41, 0, -74, 'New York', ),
+        'Mexico': Location(19, -99, 0, 'Mexico'),
+        'Rio de Janeiro': Location(-23, -43, 0, 'Rio de Janeiro'),
+        'Lima': Location(-12, -77, 221, 'Lima', ),
+        'New Delhi': Location(28, 77, 0, 'New Delhi'),
+        'Beijin': Location(22, 113, 0, 'Beijin'),
+        'Canberra': Location(-35, 149, 0, 'Canberra'),
     }
 
     locations_list = get_capitals_list(capitals_coordinates)
